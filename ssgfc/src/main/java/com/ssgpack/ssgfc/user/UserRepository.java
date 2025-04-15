a394bbc080aac49547b7f557deecdc398ac3f3a7
yoon0416 <zgha16@gmail.com>
2025-04-15 16:02:30 +0900

    usertest

---
+++ ssgfc/src/main/java/com/ssgpack/ssgfc/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ssgpack.ssgfc.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	    Optional<User> findByEmail(String email); // 이메일로 조회 (로그인 시 사용 등)
+	  //  Optional<User> findByNickName(String nick_name); 빨간줄
+}
